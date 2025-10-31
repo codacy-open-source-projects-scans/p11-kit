12bedf14edf17b6831e194511ee1104f29590a89
Codacy Archive <codacy-archive@local>
2025-10-31 02:08:21 +0000

    snapshot 2025-10-31T02:08:21.894700+00:00

---
+++ autogen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+set -e
+
+oldpwd=`pwd`
+topdir=`dirname $0`
+cd $topdir
+
+# Some boiler plate to get git setup as expected
+if test -d .git; then
+	if test -f .git/hooks/pre-commit.sample && \
+	   test ! -f .git/hooks/pre-commit; then
+		cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit
+	fi
+fi
+
+set -x
+
+autoreconf --force --install --verbose
+if test x"$NOCONFIGURE" = x; then
+  cd $oldpwd
+  exec $topdir/configure "$@"
+fi
+
